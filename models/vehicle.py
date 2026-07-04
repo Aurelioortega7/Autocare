ecd3bfb2a1221047f7a59e695c1ac129ed75a789
Aurelio Ortega <aot1004@alu.ubu.es>
2026-07-04 22:38:43 +0200

    Implementada función para registrar vehículos

---
+++ models/vehicle.py
@@ -0,0 +1,9 @@
+class Vehicle:
+
+    def __init__(self, brand, model, year, license_plate, kilometers):
+
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.license_plate = license_plate
+        self.kilometers = kilometers
