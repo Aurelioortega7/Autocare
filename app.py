1df68cd4a3182539a8123754dd000b4b7ecdfc26
Aurelio Ortega <aot1004@alu.ubu.es>
2026-07-04 22:49:07 +0200

    Implementada función para eliminar vehículos registrados

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from models.vehicle import Vehicle
+vehicles = []
 
 def register_vehicle():
     """
@@ -21,12 +22,53 @@ def register_vehicle():
         kilometers
     )
 
+    vehicles.append(vehicle)
+
     print("\n Vehículo registrado correctamente.")
-    print(f"Marca: {vehicle.brand}")
-    print(f"Modelo: {vehicle.model}")
-    print(f"Año: {vehicle.year}")
-    print(f"Matrícula: {vehicle.license_plate}")
-    print(f"Kilómetros: {vehicle.kilometers} km")
+
+def show_vehicles():
+    """
+    Muestra todos los vehículos registrados durante la ejecución del programa.
+    """
+
+    if not vehicles:
+        print("\nNo hay vehículos registrados.")
+        return
+
+    print("\n===== Vehículos registrados =====")
+
+    for index, vehicle in enumerate(vehicles, start=1):
+        print(f"\nVehículo {index}")
+        print(f"Marca: {vehicle.brand}")
+        print(f"Modelo: {vehicle.model}")
+        print(f"Año: {vehicle.year}")
+        print(f"Matrícula: {vehicle.license_plate}")
+        print(f"Kilómetros: {vehicle.kilometers} km")
+
+def delete_vehicle():
+    """
+    Elimina un vehículo registrado según su número en la lista.
+    """
+
+    if not vehicles:
+        print("\nNo hay vehículos para eliminar.")
+        return
+
+    show_vehicles()
+
+    try:
+        vehicle_number = int(input("\nIntroduce el número del vehículo a eliminar: "))
+        index = vehicle_number - 1
+
+        if index < 0 or index >= len(vehicles):
+            print("Número de vehículo no válido.")
+            return
+
+        removed_vehicle = vehicles.pop(index)
+        print(f"\nVehículo eliminado: {removed_vehicle.brand} {removed_vehicle.model}")
+
+    except ValueError:
+        print("Debes introducir un número válido.")
 
 def show_menu():
     print("\n===== AutoCare =====")
@@ -34,7 +76,8 @@ def show_menu():
     print("2. Ver vehículos")
     print("3. Registrar mantenimiento")
     print("4. Ver historial")
-    print("5. Salir")
+    print("5. Eliminar vehículo")
+    print("6. Salir")
 
 
 def main():
@@ -45,13 +88,15 @@ def main():
         if option == "1":
             register_vehicle()
         elif option == "2":
-            print("Has elegido ver vehículos")
+            show_vehicles()
         elif option == "3":
             print("Has elegido registrar mantenimiento")
         elif option == "4":
             print("Has elegido ver historial")
         elif option == "5":
-            print("Saliendo de AutoCare...")
+            delete_vehicle()
+        elif option == "6":
+            print("Saliendo de Autocare...")
             break
         else:
             print("Opción no válida. Inténtalo de nuevo.")
