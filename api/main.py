4f06c5b34e02bc8aadb71f5516bd36e2a6a9e8dd
Aurelioortega7 <aurelioortegatene222000@gmail.com>
2026-08-12 10:11:26 +0200

    permitir acceso del frontend desplegado

--- api/main.py
+++ api/main.py
@@ -13,12 +13,14 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "https://autocare-web-demo.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 @app.get("/")
 def root():
